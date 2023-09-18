64a830323c1a0258ed606872708598b2d9aac66e
lahiruprasad4858 <145246683+lahiruprasad4858@users.noreply.github.com>
2023-09-18 11:01:38 +0530

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -2,6 +2,8 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+import pandas
+import urllib.error import URLError
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
@@ -32,6 +34,8 @@ streamlit.write('The user entered ', fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
+
+streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
