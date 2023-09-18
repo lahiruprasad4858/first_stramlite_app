898f939670209414a8dd032b7931dc5e906d44a4
lahiruprasad4858 <145246683+lahiruprasad4858@users.noreply.github.com>
2023-09-18 11:11:03 +0530

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -28,6 +28,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/all")
 
 streamlit.header("Fruityvice Fruit Advice!")
 
+
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
